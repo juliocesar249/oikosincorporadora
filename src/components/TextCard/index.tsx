c1018442276b95f55126d1fef8dde02ca9e6ecc2
juliocesar249 <jc2493718@gmail.com>
2026-01-20 19:19:55 -0300

    resolvido bug visual de bordas do TextCard

--- src/components/TextCard/index.tsx
+++ src/components/TextCard/index.tsx
@@ -1,9 +1,14 @@
 import type React from "react";
+import textBorderDetail from "../../assets/img/text-border-detail.png";
 
-export function TextCard({ children, className, ...props}: { children: React.ReactNode, className?: string } & React.ComponentProps<"div">) {
+export function TextCard({ children, className, ...props }: { children: React.ReactNode, className?: string } & React.ComponentProps<"div">) {
   return (
-    <div className={`bg-[url("./assets/img/text-background.png")] bg-contain w-full p-2 pt-6 pb-6 ${className || ""}`} {...props}>
-      {children}
+    <div className={`w-full ${className || ""}`} {...props}>
+      <img src={textBorderDetail} alt="" className="w-full block" />
+      <div className="bg-white w-full px-4 py-2">
+        {children}
+      </div>
+      <img src={textBorderDetail} alt="" className="w-full block rotate-180" />
     </div>
   )
 }
