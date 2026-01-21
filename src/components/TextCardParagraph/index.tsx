d74042b15d000d9bd202f7e728bbfc8eb52b7e14
juliocesar249 <jc2493718@gmail.com>
2026-01-21 10:57:21 -0300

    resolvido problema de build no TextCardParagraph

--- src/components/TextCardParagraph/index.tsx
+++ src/components/TextCardParagraph/index.tsx
@@ -1,6 +1,6 @@
 export function TextCardParagraph({children, className, ...props} : {children: React.ReactNode} & React.ComponentProps<"p">) {
   return (
-    <p className={`px-4 py-2 ${className}`}>
+    <p className={`px-4 py-2 ${className}`} {...props}>
       {children}
     </p>
   )
